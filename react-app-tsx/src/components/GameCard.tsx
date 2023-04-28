37124626f6a268a386447e60fdcdc75a6e82798a
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-04-28 21:30:06 +0700

    Building game cards

---
+++ react-app-tsx/src/components/GameCard.tsx
@@ -0,0 +1,21 @@
+import {Game} from "../hooks/useGames";
+import {Card, CardBody, Heading, Image} from "@chakra-ui/react";
+
+interface GameCardProps {
+    game: Game
+}
+
+export const GameCard = (props: GameCardProps) => {
+    const {
+        game
+    } = props
+
+    return (
+        <Card borderRadius={10} overflow="hidden">
+            <Image src={game.background_image}/>
+            <CardBody>
+                <Heading fontSize="2xl">{game.name}</Heading>
+            </CardBody>
+        </Card>
+    )
+}
