49cd73ff4b03e7f7197451165f979a831ac1cdd5
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-05-01 11:46:49 +0700

    Filter games by genre

--- react-app-tsx/src/hooks/useGames.ts
+++ react-app-tsx/src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import {useData} from "./useData";
+import {Genre} from "./useGenres";
 export interface Platform {
     id: number,
     name: string,
@@ -11,4 +12,5 @@ export interface Game {
     parent_platforms: {platform: Platform}[],
     metacritic: number
 }
-export const useGames = () => useData<Game>('/games')
+export const useGames = (selectedGenre : Genre | null) =>
+    useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
