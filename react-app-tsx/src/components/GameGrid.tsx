44ff95fba97af09a6e3ff68fabf3b624fb022b1c
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-05-03 21:56:37 +0700

    Refactor GameGrid

--- react-app-tsx/src/components/GameGrid.tsx
+++ react-app-tsx/src/components/GameGrid.tsx
@@ -12,9 +12,8 @@ interface Props {
 export const GameGrid = ({gameQuery} : Props) => {
     const {data, error, isLoading} = useGames(gameQuery)
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    if (error) return <Text>{error}</Text>
     return (
-        <>
-            {error && <Text>{error}</Text>}
             <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
                 {isLoading && skeletons.map((skeleton) => (
                     <GameCardContainer key={skeleton}>
@@ -26,6 +25,5 @@ export const GameGrid = ({gameQuery} : Props) => {
                     </GameCardContainer>
                 ))}
             </SimpleGrid>
-        </>
     )
 }
