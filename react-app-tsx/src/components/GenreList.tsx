73fffb9124beddac676b202be457e043debd526f
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-05-03 20:17:46 +0700

    Clean up the genres

--- react-app-tsx/src/components/GenreList.tsx
+++ react-app-tsx/src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import {Genre, useGenres} from "../hooks/useGenres";
-import {Button, HStack, Image, List, ListItem, Spinner} from "@chakra-ui/react";
+import {Button, Heading, HStack, Image, List, ListItem, Spinner} from "@chakra-ui/react";
 import {getCroppedImgUrl} from "../services/image-url";
 
 interface Props {
@@ -12,18 +12,22 @@ export const GenreList = ({onSelectedGenre, selectedGenre} : Props) => {
         if (error) return null;
         if (isLoading) return <Spinner/>;
     return (
-        <List>
-            {data.map((genre) => (
-                <ListItem key={genre.id} paddingY='5px'>
-                <HStack>
-                <Image
-                    boxSize='32px'
-                    borderRadius={8}
-                    src={getCroppedImgUrl(genre.image_background)}
-                />
-                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
-                </HStack>
-            </ListItem>))}
-        </List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+            <List>
+                {data.map((genre) => (
+                    <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                    <Image
+                        objectFit='cover'
+                        boxSize='32px'
+                        borderRadius={8}
+                        src={getCroppedImgUrl(genre.image_background)}
+                    />
+                    <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                    </HStack>
+                </ListItem>))}
+            </List>
+        </>
     )
 }
