85c77614a8a8ff2893a23e6ff904202e50516257
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-05-24 18:49:31 +0700

    Setup routing

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import HomePage from "./Pages/HomePage";
+import GameDetailPage from "./Pages/GameDetailPage";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: 'games/:id', element: <GameDetailPage /> }
+        ]
+    }
+])
